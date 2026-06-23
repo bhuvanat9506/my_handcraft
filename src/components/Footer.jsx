4dceb739f1f036b6c06c875fef916e7b1876683e
bhuvanat9506 <bhuvanat9506@gmail.com>
2026-06-23 12:28:21 +0530

    Create Footer.jsx

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <footer style={{backgroundColor:"black", color:"white", textAlign:"center", padding:"24px"}}>
+      © 2026 ArtisanWood. All Rights Reserved.
+    </footer>
+  );
+}
